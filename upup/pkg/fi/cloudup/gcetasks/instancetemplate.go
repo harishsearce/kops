9723c107e842866002d8928bc87d66b543e715ca
Harish Salian <harish.salian@searce.com>
2019-02-20 01:26:15 +0530

    GPU workings

--- upup/pkg/fi/cloudup/gcetasks/instancetemplate.go
+++ upup/pkg/fi/cloudup/gcetasks/instancetemplate.go
@@ -209,6 +209,7 @@ func (e *InstanceTemplate) mapToGCE(project string) (*compute.InstanceTemplate, 
 	if (ohm_check) {
 		on_host_maintenance = on_host_maintenance_val
 	}
+	fmt.Printf("Host Maintenance %v\n", on_host_maintenance)
 	if fi.BoolValue(e.Preemptible) {
 		scheduling = &compute.Scheduling{
 			AutomaticRestart:  fi.Bool(false),
@@ -304,7 +305,7 @@ func (e *InstanceTemplate) mapToGCE(project string) (*compute.InstanceTemplate, 
 				AcceleratorType: accelerator_type,
 			})
 		}
-
+		fmt.Printf("%v\n", accelerator)
 		i := &compute.InstanceTemplate{
 			Kind: "compute#instanceTemplate",
 			Properties: &compute.InstanceProperties{
