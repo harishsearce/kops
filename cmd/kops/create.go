6437d04854f032a4846a9db1abfcf58c00be5042
Harish Salian <harish.salian@searce.com>
2019-02-20 18:29:20 +0530

    sdd

--- cmd/kops/create.go
+++ cmd/kops/create.go
@@ -180,6 +180,7 @@ func RunCreate(f *util.Factory, out io.Writer, c *CreateOptions) error {
 				fmt.Printf("4%v\n", v)
 				fmt.Printf("ObjectMeta%v\n", v.ObjectMeta)
 				fmt.Printf("Spec%v\n", v.Spec)
+				fmt.Printf("AcceleratorType%v\n", v.Spec.AcceleratorType)
 				clusterName = v.ObjectMeta.Labels[kopsapi.LabelClusterName]
 				if clusterName == "" {
 					return fmt.Errorf("must specify %q label with cluster name to create instanceGroup", kopsapi.LabelClusterName)
