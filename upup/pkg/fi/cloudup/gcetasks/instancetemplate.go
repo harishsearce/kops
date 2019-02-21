aad16e92cc2cebf379fca2571cf8d7faa66afd01
Harish Salian <harish.salian@searce.com>
2019-02-21 19:49:52 +0530

    as

--- upup/pkg/fi/cloudup/gcetasks/instancetemplate.go
+++ upup/pkg/fi/cloudup/gcetasks/instancetemplate.go
@@ -377,7 +377,7 @@ func (e *InstanceTemplate) mapToGCE(project string) (*compute.InstanceTemplate, 
 				Disks: disks,
 
 				MachineType: *e.MachineType,
-				GuestAccelerators: *e.GuestAccelerators,
+				GuestAccelerators: e.GuestAccelerators,
 				Metadata: &compute.Metadata{
 					Kind:  "compute#metadata",
 					Items: metadataItems,
