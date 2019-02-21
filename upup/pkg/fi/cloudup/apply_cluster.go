d17e08ca028db3e07084031ab1419b53ca95be70
Harish Salian <harish.salian@searce.com>
2019-02-21 15:23:49 +0530

    das

--- upup/pkg/fi/cloudup/apply_cluster.go
+++ upup/pkg/fi/cloudup/apply_cluster.go
@@ -145,6 +145,7 @@ type ApplyClusterCmd struct {
 }
 
 func (c *ApplyClusterCmd) Run() error {
+	fmt.Printf("Flow 1%v", c)
 	if c.InstanceGroups == nil {
 		list, err := c.Clientset.InstanceGroupsFor(c.Cluster).List(metav1.ListOptions{})
 		if err != nil {
@@ -156,7 +157,7 @@ func (c *ApplyClusterCmd) Run() error {
 		}
 		c.InstanceGroups = instanceGroups
 	}
-
+	fmt.Printf("Flow 2%v", c)
 	if c.Models == nil {
 		c.Models = CloudupModels
 	}
