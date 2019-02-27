05eb00d9f94c371e1164e786e745931d8057955b
Harish Salian <harish.salian@searce.com>
2019-02-27 11:55:27 +0530

    as

--- pkg/model/gcemodel/autoscalinggroup.go
+++ pkg/model/gcemodel/autoscalinggroup.go
@@ -96,8 +96,8 @@ func (b *AutoscalingGroupModelBuilder) Build(c *fi.ModelBuilderContext) error {
 				//if acverr == nil {
 				//AcceleratorCount: fi.Int64(ig.Spec.GuestAccelerators[1].AcceleratorCount),
 				accelerator = append(accelerator, &compute.AcceleratorConfig{
-					AcceleratorCount: ig.Spec.GuestAccelerators[1].AcceleratorCount,
-					AcceleratorType:  ig.Spec.GuestAccelerators[0].AcceleratorType,
+					AcceleratorCount: ig.Spec.GuestAccelerators.AcceleratorCount,
+					AcceleratorType:  ig.Spec.GuestAccelerators.AcceleratorType,
 				})
 				//}
 				//volumeSize := fi.Int32Value(ig.Spec.RootVolumeSize)
@@ -114,7 +114,7 @@ func (b *AutoscalingGroupModelBuilder) Build(c *fi.ModelBuilderContext) error {
 					CanIPForward: fi.Bool(true),
 
 					GuestAccelerators: accelerator,
-					AcceleratorType: s(ig.Spec.GuestAccelerators[0].AcceleratorType),
+					//AcceleratorType: s(ig.Spec.GuestAccelerators[0].AcceleratorType),
 
 					// TODO: Support preemptible nodes?
 					Preemptible: fi.Bool(false),
