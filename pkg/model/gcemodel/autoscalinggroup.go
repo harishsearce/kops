f10ff17e89be0ee8ff875a49bc64237d0141514b
Harish Salian <harish.salian@searce.com>
2019-02-27 11:33:12 +0530

    as

--- pkg/model/gcemodel/autoscalinggroup.go
+++ pkg/model/gcemodel/autoscalinggroup.go
@@ -113,7 +113,7 @@ func (b *AutoscalingGroupModelBuilder) Build(c *fi.ModelBuilderContext) error {
 
 					CanIPForward: fi.Bool(true),
 
-					GuestAccelerators: []*compute.AcceleratorConfig{
+					GuestAccelerators: &AcceleratorConfig{
 						AcceleratorCount: i64(int64(ig.Spec.GuestAccelerators[1].AcceleratorCount)),
 						AcceleratorType: s(ig.Spec.GuestAccelerators[0].AcceleratorType),
 					},
